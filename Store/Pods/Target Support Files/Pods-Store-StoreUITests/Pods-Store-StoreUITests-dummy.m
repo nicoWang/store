8041b96a3c838fd0c956416ce9fe2baca1ae75bb
nicolas wang <nicolas.xian@rappi.com>
2020-10-07 11:45:54 -0300

    project created

---
+++ Store/Pods/Target Support Files/Pods-Store-StoreUITests/Pods-Store-StoreUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Store_StoreUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Store_StoreUITests
+@end
---
+++ Store/Store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Store
+//
+//  Created by Nicolas Wang on 07/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
